980ad896653301c6bd55bdbf1f1004472cd0ba2b
OnurKalkan <onurkalkanv84@hotmail.com>
2024-03-10 02:44:38 +0300

    Initial commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,45 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{   
+    public int speed = 1;
+    float height = 1.25f;
+    float transSpeed = 0.25f;
+    public bool onLeft = false, mid = true, onRight = false;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.A) && onLeft == false && mid == true)
+        {
+            onLeft = true;
+            mid = false;
+            transform.DOMoveX(-2, transSpeed);
+            //transform.position = new Vector3(-2, height, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && mid == false && onRight == true)
+        {
+            onRight = false;
+            mid = true;
+            transform.DOMoveX(0, transSpeed);
+            //transform.position = new Vector3(0, height, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.D) && onRight == false && mid == true)
+        {
+            onRight = true;
+            mid = false;
+            transform.DOMoveX(2, transSpeed);
+            //transform.position = new Vector3(2, height, transform.position.z);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && onLeft == true && mid == false)
+        {
+            onLeft = false;
+            mid = true;
+            transform.DOMoveX(0, transSpeed);
+            //transform.position = new Vector3(0, height, transform.position.z);
+        }
+    }
+}

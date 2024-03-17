98e1647f9340f40b0b5981bc061bef0939a52d91
OnurKalkan <onurkalkanv84@hotmail.com>
2024-03-18 02:04:46 +0300

    Animations

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -10,16 +10,31 @@ public class Move : MonoBehaviour
     //float height = 1.25f;
     float transSpeed = 0.25f;
     public bool onLeft = false, mid = true, onRight = false;
+    int timer = 0;
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
-        //GetComponent<Rigidbody>().MovePosition(transform.position + (new Vector3(0, 0, 1) * Time.deltaTime * speed));
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Animator>().SetBool("Run", true);
+            speed = 15;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetTrigger("Jump");
+            //transform.DOJump(new Vector3(transform.position.x,2,transform.position.z), 1, 1, 0.5f);
+            transform.DOMoveY(4, 0.5f);
+            transform.DOMoveY(-0.9f, 0.5f).SetDelay(0.5f);
+        }
         if (Input.GetKeyDown(KeyCode.A) && onLeft == false && mid == true)
         {
             onLeft = true;
             mid = false;
             transform.DOMoveX(leftBorder, transSpeed);
+            GetComponent<Animator>().SetTrigger("GoLeft");
             //transform.position = new Vector3(-2, height, transform.position.z);
         }
         else if (Input.GetKeyDown(KeyCode.A) && mid == false && onRight == true)
@@ -27,6 +42,7 @@ public class Move : MonoBehaviour
             onRight = false;
             mid = true;
             transform.DOMoveX(0, transSpeed);
+            GetComponent<Animator>().SetTrigger("GoLeft");
             //transform.position = new Vector3(0, height, transform.position.z);
         }
         if (Input.GetKeyDown(KeyCode.D) && onRight == false && mid == true)
@@ -34,6 +50,7 @@ public class Move : MonoBehaviour
             onRight = true;
             mid = false;
             transform.DOMoveX(rightBorder, transSpeed);
+            GetComponent<Animator>().SetTrigger("GoRight");
             //transform.position = new Vector3(2, height, transform.position.z);
         }
         else if (Input.GetKeyDown(KeyCode.D) && onLeft == true && mid == false)
@@ -41,7 +58,22 @@ public class Move : MonoBehaviour
             onLeft = false;
             mid = true;
             transform.DOMoveX(0, transSpeed);
+            GetComponent<Animator>().SetTrigger("GoRight");
             //transform.position = new Vector3(0, height, transform.position.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "StopPoint")
+        {
+            GetComponent<Animator>().SetBool("Run", false);
+            speed = 0;
+        }
+        if (other.tag == "Obstacle")
+        {
+            GetComponent<Animator>().SetBool("Die", true);
+            speed = 0;
         }
     }
 }
